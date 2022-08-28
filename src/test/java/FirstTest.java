edb51bd19465e889f49b0ea4c3ef23a2bef4496f
berikca2022 <110699514+berikca2022@users.noreply.github.com>
2022-08-28 01:01:54 -0700

    first app test

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -3,9 +3,17 @@ import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
+import java.nio.file.WatchEvent;
+import java.util.List;
+import java.util.Set;
 
 public class FirstTest {
 
@@ -33,6 +41,22 @@ public class FirstTest {
 
     @Test
     public void firstTest(){
-        System.out.println("first test");
+        WebElement skip_button = driver.findElementByXPath("//*[contains(@text, 'SKIP')]");
+        skip_button.click();
+
+        WebElement search_field = driver.findElementByXPath("//*[contains(@text, 'Search Wikipedia')]");
+        search_field.click();
+
+        WebElement enter_text = waitElementById("search_src_text", "Cannot find input field", 5);
+        enter_text.sendKeys("abc");
+
+    }
+
+    private WebElement waitElementById(String id, String error_message, long timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message + "\n");
+        By by = By.id(id);
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
+
     }
 }
